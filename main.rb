9032413395597b01f7ce0165244e8bda78085937
“Richard <richgurney1987@gmail.com>
2016-03-07 18:22:33 +0000

    initial commit

---
+++ main.rb
@@ -0,0 +1,66 @@
+# calls the sinatra tools
+require "sinatra"
+require "sinatra/reloader" if development?
+
+# when get an home are run it sets the @translate to string
+get "/" do
+	@translate = "Pick your word to translate"
+	erb :home
+end
+
+# when the post home is run it set the chain reaction off with 
+# the method to chnage the value of @translate
+post "/" do
+
+	# array to push the translated words to
+	pig_translation = []
+
+	# makes the user input an array with 
+	@translate = params[:translate].downcase.split(" ")
+
+
+	# goes through each word in array and performs translation
+	@translate.each do |word|
+	    
+	    if word.start_with?("a", "e", "i", "o", "u")
+	        @translate = "#{word}ay"
+	    else
+	        start_letter = word.slice(0)
+	        word.slice!(0)
+	        @translate = "#{word}#{start_letter}ay"
+	    end
+
+	    # puts the result to the translation array
+	    pig_translation.push(@translate)
+	    
+	end
+
+	#this makes the array a string again
+	@translate = pig_translation.join(" ")
+
+	erb :home
+
+end
+
+
+# Sudo for bonus
+# word = "and the thing"
+
+# pig_array = []
+
+# new word = word.(something to send to array with spaces)
+
+# new word.each do |word|
+
+# if word.start_with?("a", "e", "i", "o", "u")
+# 	        send this to array array "#{word}ay"
+# 	    else
+# 	        start_letter = word.slice(0)
+# 	        word.slice!(0)
+# 	        send this to pg array"#{word}#{start_letter}ay"
+# 	    end
+
+# @translate = pig_array into a string
+
+
+# end 
